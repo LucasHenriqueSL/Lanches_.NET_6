b50aea55309b6cb8e0c1770f5844a4a03010d558
LucasHenriqueSL <lucashenrique8736@gmail.com>
2022-10-06 10:56:54 -0300

    2.0

--- Lanches/Models/Lanche.cs
+++ Lanches/Models/Lanche.cs
@@ -1,9 +1,23 @@
-﻿namespace Lanches.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Lanches.Models
 {
+    [Table("Lanches")]
     public class Lanche
     {
+        [Key]
         public int LancheId { get; set; }
+        [Required(ErrorMessage ="O nome do lanche deve ser informado")]
+        [Display(Name ="Nome do Lanche")]
+        [StringLength(80, MinimumLength =10,ErrorMessage ="O {0} deve ter no mínimo {1} e no máximo {2}")]
+
         public string Nome { get; set; }
+        [Required(ErrorMessage = "O nome do lanche deve ser informado")]
+        [Display(Name = "Nome do Lanche")]
+        [MinLength(20, ErrorMessage = "Descrição deve ter no mínimo {1}")]
+        [MaxLength(200, ErrorMessage = "Descrição pode exceder {1} caracteres")]
+
         public string DescricaoCurta { get; set; }
         public string DescricaoDetalhada { get; set; }
         public decimal Preco { get; set; }
