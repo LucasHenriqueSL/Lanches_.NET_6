6554c12a9e96feec3960e319be22619753eb9af8
LucasHenriqueSL <lucashenrique8736@gmail.com>
2022-10-06 11:43:00 -0300

    Data Annotations

--- Lanches/Models/Lanche.cs
+++ Lanches/Models/Lanche.cs
@@ -14,16 +14,32 @@ namespace Lanches.Models
 
         public string Nome { get; set; }
         [Required(ErrorMessage = "O nome do lanche deve ser informado")]
-        [Display(Name = "Nome do Lanche")]
+        [Display(Name = "Descrição do Lanche")]
         [MinLength(20, ErrorMessage = "Descrição deve ter no mínimo {1}")]
         [MaxLength(200, ErrorMessage = "Descrição pode exceder {1} caracteres")]
 
         public string DescricaoCurta { get; set; }
+        [Required(ErrorMessage = "A descrição detalhada do lanche deve ser informado")]
+        [Display(Name = "descrição detalhada do Lanche")]
+        [MinLength(20, ErrorMessage = "Descrição deve ter no mínimo {1}")]
+        [MaxLength(200, ErrorMessage = "Descrição pode exceder {1} caracteres")]
         public string DescricaoDetalhada { get; set; }
+        [Required(ErrorMessage = "Informe o preço do lanche")]
+        [Display(Name = "Preço")]
+        [Column(TypeName ="decimal(10,2)")]
+        [Range(1,999.99,ErrorMessage ="O preço deve estar entre 1 e 999,99")]
+
         public decimal Preco { get; set; }
+        [Display(Name = "Caminho imagem normal")]
+        [StringLength(200,ErrorMessage = "O {0} deve ter no máximo {1}")]
+
         public string ImagemUrl { get; set; }
+        [Display(Name = "Caminho imagem Miniatura")]
+        [StringLength(200, ErrorMessage = "O {0} deve ter no máximo {1}")]
         public string ImagemThumbnailUrl { get; set; }
+        [Display(Name = "Preferido")]
         public bool IsLanchePreferido { get; set; }
+        [Display(Name = "Estoque")]
         public bool EmEstoque { get; set; }
 
         public int CategoriaId { get; set; }
