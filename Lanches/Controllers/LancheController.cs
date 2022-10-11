3a0276b444023a6a65ab6001e8dae9857037f434
LucasHenriqueSL <lucashenrique8736@gmail.com>
2022-10-11 11:55:16 -0300

    Updates

--- Lanches/Controllers/LancheController.cs
+++ Lanches/Controllers/LancheController.cs
@@ -16,14 +16,9 @@ namespace Lanches.Controllers
         public IActionResult List()
         {
 
-            ViewData["Titulo"] = "Todos os Lanches";
-            ViewData["Data"] = DateTime.Now;
+           
 
             var lanches = _lancheRepository.Lanches;
-            var totalLanches = lanches.Count();
-
-            ViewBag.Total = "Total de Lanches: ";
-            ViewBag.TotalLanches = totalLanches;
 
             return View(lanches);
         }
